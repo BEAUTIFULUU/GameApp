4fdb1f95cef9a6216090d4818a2f1c8c1446ef91
8EAUTIFUL <pawel.wardachpr@gmail.com>
2024-02-13 15:18:54 +0100

    Management services tests added

--- tests/test_management_services.py
+++ tests/test_management_services.py
@@ -4,6 +4,8 @@ from management_services.game_records_management import (
     get_user_game_records,
     update_game_record,
 )
+from management_services.login_acc import login_into_acc
+from management_services.register_acc import register_acc
 
 
 @pytest.fixture
@@ -12,6 +14,14 @@ def open_records_dict():
         filename="tests/test_data/test_user_records.json", dict_key="records"
     )
     return records_dict
+
+
+@pytest.fixture
+def open_accounts_dict():
+    accounts_dict = read_data_from_file(
+        filename="tests/test_data/test_accounts.json", dict_key="accounts"
+    )
+    return accounts_dict
 
 
 class TestGamesRecordsManagement:
@@ -118,3 +128,101 @@ class TestGamesRecordsManagement:
             user_records=records_dict,
         )
         assert username not in records_dict
+
+
+class TestLoginManagement:
+    def test_login_into_acc_return_true_if_username_and_password_are_valid(
+        self, open_accounts_dict
+    ):
+        accounts = open_accounts_dict
+        username = "testusername55"
+        password = "testpassword354"
+        result = login_into_acc(username=username, password=password, accounts=accounts)
+        assert result is True
+
+    def test_login_into_acc_return_false_if_username_not_valid(
+        self, open_accounts_dict
+    ):
+        accounts = open_accounts_dict
+        username = "123"
+        password = "testpassword354"
+        result = login_into_acc(username=username, password=password, accounts=accounts)
+        assert result is False
+
+    def test_login_into_acc_return_false_if_password_not_valid(
+        self, open_accounts_dict
+    ):
+        accounts = open_accounts_dict
+        username = "testusername55"
+        password = "123"
+        result = login_into_acc(username=username, password=password, accounts=accounts)
+        assert result is False
+
+    def test_login_into_acc_return_false_if_username_and_password_not_valid(
+        self, open_accounts_dict
+    ):
+        accounts = open_accounts_dict
+        username = "123"
+        password = "123"
+        result = login_into_acc(username=username, password=password, accounts=accounts)
+        assert result is False
+
+
+class TestRegisterManagement:
+    def test_register_acc_return_account_data_and_message_if_username_login_password_are_valid(
+        self, open_accounts_dict
+    ):
+        accounts = open_accounts_dict
+        username = "testusername44"
+        login = "testlogin21"
+        password = "testpassword453"
+        result, message = register_acc(
+            username=username, login=login, password=password, accounts=accounts
+        )
+        assert message == "Account registered."
+        assert username in accounts
+        assert accounts[username]["login"] == login
+        assert accounts[username]["password"] == password
+        assert accounts[username]["token"] is not None
+
+    def test_register_acc_return_false_and_error_message_if_username_invalid(
+        self, open_accounts_dict
+    ):
+        accounts = open_accounts_dict
+        username = "1"
+        login = "testlogin21"
+        password = "testpassword453"
+        result, message = register_acc(
+            username=username, login=login, password=password, accounts=accounts
+        )
+        assert result is False
+        assert "Invalid username" in message
+        assert username not in accounts
+
+    def test_register_acc_return_false_and_error_message_if_login_invalid(
+        self, open_accounts_dict
+    ):
+        accounts = open_accounts_dict
+        username = "testusername44"
+        login = "1"
+        password = "testpassword453"
+        result, message = register_acc(
+            username=username, login=login, password=password, accounts=accounts
+        )
+        assert result is False
+        assert "Invalid login" in message
+        assert username not in accounts
+
+    def test_register_acc_return_false_and_error_message_if_password_invalid(
+        self, open_accounts_dict
+    ):
+        accounts = open_accounts_dict
+        username = "testusername44"
+        login = "testlogin21"
+        password = "1"
+        result, message = register_acc(
+            username=username, login=login, password=password, accounts=accounts
+        )
+        assert result is False
+        assert "Invalid password" in message
+        assert username not in accounts
